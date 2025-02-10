0202156ed3c043edddeeefd9084f5c87f3d9e189
soprane <sandrine.houston.metayer@gmail.com>
2025-02-10 09:26:22 +0100

    Update Grille.java

--- Grille.java
+++ Grille.java
@@ -2,6 +2,9 @@
  * The Grille class in Java represents a grid structure with methods for initializing, modifying,
  * displaying, and comparing grids.
  */
+
+/* c'est la classe */
+
 public class Grille {    
     public StringBuilder grille[];
 
